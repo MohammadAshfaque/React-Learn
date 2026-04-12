30cea19fe145e14f68f690f0cde11999c3a0e6ef
codewithashfaque <mohammadashfaque18@gmail.com>
2026-04-12 15:43:45 +0530

    add item

--- src/components/Card1.jsx
+++ src/components/Card1.jsx
@@ -13,10 +13,10 @@ function Card1() {
     ];
   return (
     <div className='w-full h-screen bg-zinc-300 flex flex-col gap-4 justify-center items-center'>
-        {data.map((elem, index)=>(
+        {data.map((item, index)=>(
             <div className='w-90 px-3 py-2 bg-zinc-100 rounded-md'>
-            <h3 className='font-semibold text-xl '>{elem.name}</h3>
-            <p className='text-xs mt-2'>{elem.description}</p>
+            <h3 className='font-semibold text-xl '>{item.name}</h3>
+            <p className='text-xs mt-2'>{item.description}</p>
             <button onClick={handleClickDownload} className='px-2 py-1 bg-blue-400 text-xs font-semibold text-zinc-100 rounded mt-3'>Download Now</button>
         </div>
         ))}
