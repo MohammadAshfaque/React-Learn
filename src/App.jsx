10081d29b5d0f114660f59a1094ca731b575e3f9
codewithashfaque <mohammadashfaque18@gmail.com>
2026-04-16 13:53:18 +0530

    learn React js Dynamic Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
-import React, { useState } from 'react';
-import Cardsf from './components/Cardsf'
-import Form from './components/Form';
+// import React, { useState } from 'react';
+// import Cardsf from './components/Cardsf'
+// import Form from './components/Form';
 
-function App() {
-  const [users, setUsers]=  useState([]);
+// function App() {
+//   const [users, setUsers]=  useState([]);
 
-  const handleFormSubmitData = (data)=>{
-    setUsers([...users , data])
-  }
+//   const handleFormSubmitData = (data)=>{
+//     setUsers([...users , data])
+//   }
   
-  const handleRemove = (id)=>{
-   setUsers(()=>users.filter((item , index)=>index!=id))
-  }
+//   const handleRemove = (id)=>{
+//    setUsers(()=>users.filter((item , index)=>index!=id))
+//   }
 
+//   return (
+//     <div className='w-full h-screen bg-zinc-200 flex items-center justify-center'>
+//         <div className="container mx-auto">
+//             <Cardsf handleRemove={handleRemove} users={users}/>
+//             <Form handleFormSubmitData={handleFormSubmitData}/>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+import Nav from './components/Nav';
+import Routing from './utils/Routing';
+
+const App=()=> {
   return (
-    <div className='w-full h-screen bg-zinc-200 flex items-center justify-center'>
-        <div className="container mx-auto">
-            <Cardsf handleRemove={handleRemove} users={users}/>
-            <Form handleFormSubmitData={handleFormSubmitData}/>
-        </div>
-    </div>
+    <>
+      <Nav/>
+      <Routing/>
+    </>
   )
 }
 
---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const  Home=()=> {
+  return (
+    <div className="w-1/2 m-auto mt-10">
+        <h1 className='text-red-200 text-5xl mb-2'>Home</h1>
+
+        <p className='mb-2 text-zinc-500'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A porro fuga id.</p>
+        <button className='text-white p-3 bg-zinc-300'>Explore button</button>
+    </div>
+  )
+}
+
+export default Home
